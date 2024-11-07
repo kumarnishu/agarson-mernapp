be77591d0713d28d13423d073fc87a27ccac1c37
nishu kumar <kumarnishu437@gmail.com>
2024-11-07 12:18:04 +0530

    commit

--- controllers/crm.reports.ts
+++ controllers/crm.reports.ts
@@ -91,7 +91,7 @@ export const GetMyReminders = async (req: Request, res: Response, next: NextFunc
     let previous_date = new Date()
     let day = previous_date.getDate() - 100
     previous_date.setDate(day)
-    let remarks = await Remark.find({ created_at: { $gte: previous_date, $lte: new Date(today) } }).populate('created_by').populate('updated_by').populate({
+    let remarks = await Remark.find({ created_at: { $gte: previous_date, $lte: new Date() } }).populate('created_by').populate('updated_by').populate({
         path: 'lead',
         populate: [
             {
@@ -112,13 +112,14 @@ export const GetMyReminders = async (req: Request, res: Response, next: NextFunc
     let ids: string[] = []
     let filteredRemarks: IRemark[] = []
 
-    remarks.forEach((rem) => {
+    for(let i=0;i<=remarks.length;i++){
+        let rem=remarks[i]
         if (rem && rem.lead && !ids.includes(rem.lead._id)) {
             ids.push(rem.lead._id);
-            if (rem.created_by._id.valueOf() == req.user?._id && rem.remind_date && rem.remind_date >= hundredDaysAgo && rem.remind_date < today)
+            if (rem.created_by._id.valueOf() == req.user?._id && rem.remind_date && rem.remind_date >= hundredDaysAgo && rem.remind_date < new Date())
                 filteredRemarks.push(rem);
         }
-    })
+    }
     filteredRemarks.sort(function (a, b) {
         //@ts-ignore
         return new Date(b.remind_date) - new Date(a.remind_date);
