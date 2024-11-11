e1fd93c484da4ed9c742efae3a62adac3bcba694
nishu kumar <kumarnishu437@gmail.com>
2024-11-11 08:06:01 +0530

    commit

--- utils/activateChecklist.ts
+++ utils/activateChecklist.ts
@@ -5,38 +5,43 @@ import { Checklist } from "../models/checklist";
 export async function activateChecklist() {
 
     // daily trigger
-    new CronJob("0 0 1/1 * *", async () => {
+    new CronJob("15 8 1/1 * *", async () => {
         let works = await Checklist.find({ active: false, frequency: 'daily' });
         for (let i = 0; i < works.length; i++) {
             let work = works[i]
-            await Checklist.findByIdAndUpdate(work._id, { active: true , lastcheckedbox:undefined})
+            await Checklist.findByIdAndUpdate(work._id, { active: true})
         }
+        
+        await Checklist.updateMany({frequency:"daily"},{lastcheckedbox: undefined })
     }).start()
 
     //weekly
-    new CronJob("0 0 * * 1", async () => {
+    new CronJob("15 8 * * 1", async () => {
         let works = await Checklist.find({ active: false, frequency: 'weekly' })
         for (let i = 0; i < works.length; i++) {
             let work = works[i]
-            await Checklist.findByIdAndUpdate(work._id, { active: true , lastcheckedbox:undefined})
+            await Checklist.findByIdAndUpdate(work._id, { active: true})
         }
+        await Checklist.updateMany({ frequency:"weekly"},{lastcheckedbox: undefined })
     }).start()
 
     //monthly
-    new CronJob("0 0 1 * *", async () => {
+    new CronJob("15 8 1 * *", async () => {
         let works = await Checklist.find({ active: false, frequency: 'monthly' })
         for (let i = 0; i < works.length; i++) {
              let work = works[i];
-            await Checklist.findByIdAndUpdate(work._id, { active: true , lastcheckedbox:undefined})
+            await Checklist.findByIdAndUpdate(work._id, { active: true})
         }
+        await Checklist.updateMany({ frequency:"monthly"},{lastcheckedbox: undefined })
     }).start()
 
     //yearly
-    new CronJob("0 0 1 1 *", async () => {
+    new CronJob("15 8 1 1 *", async () => {
         let works = await Checklist.find({ active: false, frequency: 'yearly' })
         for (let i = 0; i < works.length; i++) {
              let work = works[i];
-            await Checklist.findByIdAndUpdate(work._id, { active: true , lastcheckedbox:undefined})
+            await Checklist.findByIdAndUpdate(work._id, { active: true })
         }
+        await Checklist.updateMany({ frequency: "yearly" }, { lastcheckedbox: undefined })
     }).start()
 }
