efbaad24fa7d7c27150a22d19990850266b57351
nishu kumar <kumarnishu437@gmail.com>
2024-12-11 17:29:08 +0530

    commit

--- controllers/visit-report.controller.ts
+++ controllers/visit-report.controller.ts
@@ -9,7 +9,7 @@ import { KeyCategory } from '../models/key-category.model';
 
 
 export const GetVisitReports = async (req: Request, res: Response, next: NextFunction) => {
-    const category=req.query.category
+    const category = req.query.category
     let dt1 = new Date()
     dt1.setHours(0, 0, 0, 0)
     let dt2 = new Date()
@@ -18,17 +18,21 @@ export const GetVisitReports = async (req: Request, res: Response, next: NextFun
     dt2.setHours(0)
     dt2.setMinutes(0)
     let cat = await KeyCategory.findOne({ category: category })
-    let remarks = await ExcelDBRemark.find({ 
-        created_at: { $gte: dt1, $lt: dt2 }, 
-        category: cat 
+    let remarks = await ExcelDBRemark.find({
+        created_at: { $gte: dt1, $lt: dt2 },
+        category: cat
     })
-    .populate({
-        path: 'created_by', 
-        select: 'username' // Specify the fields to fetch from the populated model
-    })
-    .select('remark obj created_at');
-    
-    return res.status(200).json(remarks);
+        .populate({
+            path: 'created_by',
+            select: 'username' // Specify the fields to fetch from the populated model
+        })
+        .select('remark obj created_at');
+
+    return res.status(200).json({
+        total: await ExcelDBRemark.find({ category: cat }).count(),
+        today: remarks.length,
+        remarks: remarks
+    });
 }
 
 
