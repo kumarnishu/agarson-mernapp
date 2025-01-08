7b823d0f7eded4af7b24fbd1ab844a7ccdb7c17a
nishu kumar <kumarnishu437@gmail.com>
2025-01-08 13:02:58 +0530

    commit

--- client/src/components/dashboards/ExcelDBDashboard.tsx
+++ client/src/components/dashboards/ExcelDBDashboard.tsx
@@ -1,4 +1,4 @@
-import { Grid, Paper, Stack, Typography } from "@mui/material"
+import { Grid, LinearProgress, Paper, Stack, Typography } from "@mui/material"
 import { Link, Outlet } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import { AxiosResponse } from "axios";
@@ -13,7 +13,7 @@ import { AuthorizationService } from "../../services/AuthorizationService";
 function ExcelDBDashboard() {
     const [features, setFeatures] = useState<{ feature: string, display_name: string, is_visible: boolean, url: string }[]>([])
     const { user } = useContext(UserContext)
-    const { data: categoryData } = useQuery<AxiosResponse<DropDownDto[]>, BackendError>(["key_category_dropdown"], async () => new AuthorizationService().GetAllKeyCategoriesForDropdown({ show_assigned_only: true }))
+    const { data: categoryData, isLoading } = useQuery<AxiosResponse<DropDownDto[]>, BackendError>(["key_category_dropdown"], async () => new AuthorizationService().GetAllKeyCategoriesForDropdown({ show_assigned_only: true }))
 
 
     useEffect(() => {
@@ -35,49 +35,50 @@ function ExcelDBDashboard() {
             {user?.assigned_permissions.includes('grp_excel_create') && <Stack sx={{ width: '100%' }} direction={'row'} p={2} justifyContent={'end'}>
                 <ExcelDbButtons />
             </Stack>}
-          <Grid container  >
-                         {features.map((feat, index) => {
-                             return (
-                                 <Grid key={index} item xs={12} md={4} lg={4} sx={{ p: 1 }}>
-                                     <Link to={feat.url} style={{ textDecoration: 'none' }}>
-                                         <Paper
-                                             sx={{
-                                                 p: 2,
-                                                 m: 0,
-                                                 minHeight: 60,
-                                                 position: 'relative',
-                                                 overflow: 'hidden',
-                                                 backdropFilter: 'blur(10px)', // Blurry effect
-                                                 backgroundColor: 'rgba(255, 255, 255, 0.6)', // Semi-transparent blue
-                                                 transition: '0.3s',
-                                                 '&:hover': {
-                                                     transform: 'translateY(-2px)',
-                                                     backgroundColor: 'rgba(70, 130, 180, 0.7)', // Darken on hover
-                                                 },
-                                             }}
-                                         >
-                                             <Stack
-                                                 flexDirection="row"
-                                                 gap={2}
-                                                 sx={{ alignItems: 'center' }}
-                                             >
-                                                 <AssignmentOutlined />
-                                                 <Typography
-                                                     variant="h6"
-                                                     component="div"
-                                                     sx={{
-                                                         fontWeight: 'medium', fontSize: 14
-                                                     }}
-                                                 >
-                                                     {feat.feature}
-                                                 </Typography>
-                                             </Stack>
-                                         </Paper>
-                                     </Link>
-                                 </Grid>
-                             )
-                         })}
-                     </Grid>
+            {isLoading && <LinearProgress />}
+            <Grid container  >
+                {features.map((feat, index) => {
+                    return (
+                        <Grid key={index} item xs={12} md={4} lg={4} sx={{ p: 1 }}>
+                            <Link to={feat.url} style={{ textDecoration: 'none' }}>
+                                <Paper
+                                    sx={{
+                                        p: 2,
+                                        m: 0,
+                                        minHeight: 60,
+                                        position: 'relative',
+                                        overflow: 'hidden',
+                                        backdropFilter: 'blur(10px)', // Blurry effect
+                                        backgroundColor: 'rgba(255, 255, 255, 0.6)', // Semi-transparent blue
+                                        transition: '0.3s',
+                                        '&:hover': {
+                                            transform: 'translateY(-2px)',
+                                            backgroundColor: 'rgba(70, 130, 180, 0.7)', // Darken on hover
+                                        },
+                                    }}
+                                >
+                                    <Stack
+                                        flexDirection="row"
+                                        gap={2}
+                                        sx={{ alignItems: 'center' }}
+                                    >
+                                        <AssignmentOutlined />
+                                        <Typography
+                                            variant="h6"
+                                            component="div"
+                                            sx={{
+                                                fontWeight: 'medium', fontSize: 14
+                                            }}
+                                        >
+                                            {feat.feature}
+                                        </Typography>
+                                    </Stack>
+                                </Paper>
+                            </Link>
+                        </Grid>
+                    )
+                })}
+            </Grid>
             <Outlet />
         </>
 
