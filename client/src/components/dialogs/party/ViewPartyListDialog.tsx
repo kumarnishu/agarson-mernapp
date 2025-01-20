e697f8960b5773041d5454598d4af2dae8cdb3f5
nishu kumar <kumarnishu437@gmail.com>
2025-01-20 11:12:43 +0530

    commit

--- client/src/components/dialogs/party/ViewPartyListDialog.tsx
+++ client/src/components/dialogs/party/ViewPartyListDialog.tsx
@@ -6,10 +6,11 @@ type Props = {
     dialog: string | undefined,
     setDialog: React.Dispatch<React.SetStateAction<string | undefined>>
 }
-function ViewPartyListDialog({  dialog, setDialog }: Props) {
+function ViewPartyListDialog({ dialog, setDialog }: Props) {
     return (
         <Dialog
             open={dialog === "ViewPartyListDialog"}
+            onClose={() => { setDialog(undefined) }}
         >
             <IconButton style={{ display: 'inline-block', position: 'absolute', right: '0px' }} color="error" onClick={() => {
                 setDialog(undefined)
